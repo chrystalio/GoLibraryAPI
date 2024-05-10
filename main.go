54375f29216d9b4fa8231899f4e2734781adc3e8
Chrystalio <chrystalio.k@outlook.com>
2024-05-10 23:57:37 +0700

    Set timeout config

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", hello)
 
-	http.ListenAndServe(":3000", mux)
+	s := &http.Server{
+		Addr:         ":3000",
+		Handler:      mux,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+		IdleTimeout:  5 * time.Second,
+	}
+
+	s.ListenAndServe()
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
